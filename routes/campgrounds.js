e8d37ed7f5b83556613819a4b470dd6cb921c092
Thomas Dean <tomdean22@gmail.com>
2019-08-11 19:01:13 -0500

    Clean-up and restricting access

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,13 +14,17 @@ router.get("/", function(req, res){
     });
 });
 
-router.post("/", function(req, res) {
+router.post("/", isLoggedIn, function(req, res) {
     // get data from form and add to campgrounds array
     // redirect back to campgrounds page
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name: name, image: image, description: desc, author: author};
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated) {
         if (err){
@@ -31,7 +35,7 @@ router.post("/", function(req, res) {
     });
 });
 
-router.get("/new", function(req, res) {
+router.get("/new", isLoggedIn, function(req, res) {
     res.render("campgrounds/new");
 });
 
@@ -47,5 +51,13 @@ router.get("/:id", function(req, res){
         }
     });
 });
+
+// middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
--- routes/comments.js
+++ routes/comments.js
@@ -28,6 +28,13 @@ router.post("/", isLoggedIn, function(req, res){
                 if (err){
                     console.log(err);
                 } else {
+                    // add username and id to comment
+                    console.log("New comment's username will be: " + req.user.username);
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    // save comment
+                    comment.save();
+                    console.log(comment);
                     campground.comments.push(comment);
                     campground.save();
                     res.redirect("/campgrounds/" + campground._id);
